82bbe0e54620d971aef28e37bc7e6905c7f18110
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-09 21:52:49 +0545

    Update writing_queries.sql

--- starter/writing_queries.sql
+++ starter/writing_queries.sql
@@ -30,3 +30,13 @@ CREATE VIEW library_authors AS
 SELECT DISTINCT author AS unique_author
 FROM books;
 
+
+--2.Check that the view was created by selecting all columns from library_authors.
+-- Your code to create the view:
+CREATE VIEW library_authors AS
+SELECT DISTINCT author AS unique_author
+FROM books;
+
+-- Select all columns from library_authors
+SELECT *
+FROM library_authors
