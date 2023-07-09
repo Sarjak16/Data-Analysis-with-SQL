a3c3916b5876d993b29365b5cfb350a251497d83
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-09 21:47:27 +0545

    Update writing_queries.sql

--- starter/writing_queries.sql
+++ starter/writing_queries.sql
@@ -2,3 +2,9 @@
 --  But how many different authors are represented in these 350 books? The answer is surely less than 350. For example, J.K.
 --   Rowling wrote all seven Harry Potter books,
 --  so if our library has all Harry Potter books, seven books will be written by J.K Rowling. There are likely many more repeat authors!
+
+-- Questions
+-- 1.Write SQL code that returns a result set with just one column listing the unique authors in the books table.
+-- Select unique authors from the books table
+SELECT DISTINCT author
+FROM books
