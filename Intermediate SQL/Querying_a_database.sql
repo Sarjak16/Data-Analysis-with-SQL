de5057b5da26606d64121186c08a4222cfbf857a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-12 20:51:02 +0545

    Update Querying_a_database.sql

---
+++ Intermediate SQL/Querying_a_database.sql
@@ -0,0 +1,10 @@
+-- 1.
+-- Count the total number of records in the people table, aliasing the result as count_records.
+-- Count the number of records in the people table
+SELECT COUNT(*)AS count_records
+FROM people
+
+-- 2.
+-- Count the number of records with a birthdate in the people table, aliasing the result as count_birthdate.
+-- 3.
+-- Count the records for languages and countries in the films table; alias as count_languages and count_countries.
