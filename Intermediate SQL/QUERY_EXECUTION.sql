8689bd9f77dcd00173df4bac1f039194761231e0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-13 21:09:19 +0545

    Update QUERY_EXECUTION.sql

--- Intermediate SQL/QUERY_EXECUTION.sql
+++ Intermediate SQL/QUERY_EXECUTION.sql
@@ -1,1 +1,16 @@
+-- 1.  
+--Debug and fix the SQL query provided.
+-- SELECT certfication
+-- FROM films
+-- LIMIT 5;
 
+-- Debug this code
+SELECT certification
+FROM films
+LIMIT 5;
+
+
+-- 2
+-- Find the two errors in this code; the same error has been repeated twice.
+-- 3
+-- Find the two bugs in this final query.
