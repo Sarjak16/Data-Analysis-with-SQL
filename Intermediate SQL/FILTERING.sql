31a452c013030d7860a861f08c23e1af373efd88
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-16 14:25:08 +0545

    Update FILTERING.sql

--- Intermediate SQL/FILTERING.sql
+++ Intermediate SQL/FILTERING.sql
@@ -6,5 +6,9 @@ WHERE imdb_score > 7.0;
 
 -- 2
 -- Select the film_id and facebook_likes of the first ten records with less than 1000 likes from the reviews table.
+SELECT film_id, facebook_likes
+FROM reviews
+WHERE facebook_likes < 1000
+LIMIT 10;
 -- 3
 -- Count how many records have a num_votes of at least 100,000; use the alias films_over_100K_votes.
