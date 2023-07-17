16dae519f7e12d1c93e8d3b5dee31d5e95a32424
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-17 22:36:42 +0545

    Update Multiple_criteria.sql

--- Intermediate SQL/Multiple_criteria.sql
+++ Intermediate SQL/Multiple_criteria.sql
@@ -56,3 +56,12 @@ FROM films
 WHERE release_year BETWEEN 1990 AND 2000
 -- Narrow down your query to films with budgets > $100 million
 	AND budget > 100000000;;
+
+
+--Now, restrict the query to only return Spanish-language films.
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Restrict the query to only Spanish-language films
+	AND(language='Spanish');
