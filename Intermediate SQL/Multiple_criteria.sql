b484ab75db428482ce642f32344c4ba5eea225e8
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-17 22:19:40 +0545

    Update Multiple_criteria.sql

--- Intermediate SQL/Multiple_criteria.sql
+++ Intermediate SQL/Multiple_criteria.sql
@@ -7,5 +7,10 @@ WHERE language= 'German' AND release_year<2000
 
 -- 2
 -- Update the query from the previous step to show German-language films released after 2000 rather than before.
+-- Update the query to see all German-language films released after 2000
+SELECT title, release_year
+FROM films
+WHERE release_year > 2000
+	AND language = 'German';
 -- 3
 -- Select all details for German-language films released after 2000 but before 2010 using only WHERE and AND.
