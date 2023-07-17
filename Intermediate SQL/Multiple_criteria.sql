5c06c41005105ba25fdb61a0ddde5bad3d5d8019
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-17 22:34:40 +0545

    Update Multiple_criteria.sql

--- Intermediate SQL/Multiple_criteria.sql
+++ Intermediate SQL/Multiple_criteria.sql
@@ -42,3 +42,10 @@ WHERE (release_year = 1990 OR release_year = 1999)
 	AND (language = 'English' OR language = 'Spanish')
 -- Filter films with more than $2,000,000 gross
 	AND gross > 2000000;;
+
+-- ............................................................................................................................................................................]
+-- Select the title and release_year of all films released between 1990 and 2000 (inclusive) using BETWEEN.
+-- Select the title and release_year for films released between 1990 and 2000
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000;
