100a3ba88d5b6b6d04d46fd35595bd02abd77c97
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-17 22:33:31 +0545

    Update Multiple_criteria.sql

--- Intermediate SQL/Multiple_criteria.sql
+++ Intermediate SQL/Multiple_criteria.sql
@@ -33,3 +33,12 @@ FROM films
 WHERE (release_year = 1990 OR release_year = 1999)
 -- Add a filter to see only English or Spanish-language films
 AND	((language='spanish' OR language='English'));
+
+-- Finally, restrict the query to only return films worth more than $2,000,000 gross.
+
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+	AND (language = 'English' OR language = 'Spanish')
+-- Filter films with more than $2,000,000 gross
+	AND gross > 2000000;;
