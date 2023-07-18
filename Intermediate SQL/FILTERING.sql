0a5cf3aaa98fa79787f7253fde4e3cd1d7466260
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-18 20:45:37 +0545

    Update FILTERING.sql

--- Intermediate SQL/FILTERING.sql
+++ Intermediate SQL/FILTERING.sql
@@ -59,3 +59,7 @@ FROM films
 WHERE language IN ('English','Spanish','French');
 -- 3
 -- Select the title, certification and language of all films certified NC-17 or R that are in English, Italian, or Greek.
+-- Find the title, certification, and language all films certified NC-17 or R that are in English, Italian, or Greek
+SELECT title, certification, language
+FROM films
+WHERE certification IN ('NC-17','R') AND language IN ('English','Italian','Greek');
