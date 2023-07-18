0ad947bda42a8d04778f68fc9171b1c5c8a55cd2
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-18 20:49:09 +0545

    Update FILTERING.sql

--- Intermediate SQL/FILTERING.sql
+++ Intermediate SQL/FILTERING.sql
@@ -63,3 +63,21 @@ WHERE language IN ('English','Spanish','French');
 SELECT title, certification, language
 FROM films
 WHERE certification IN ('NC-17','R') AND language IN ('English','Italian','Greek');
+
+
+
+-- ..............................................FINAL........................................
+--   Count the unique titles from the films database and use the alias provided.
+-- Filter to include only movies with a release_year from 1990 to 1999, inclusive.
+-- Add another filter narrowing your query down to English-language films.
+-- Add a final filter to select only films with 'G', 'PG', 'PG-13' certifications.
+
+-- Count the unique titles
+SELECT  COUNT(DISTINCT(title)) AS nineties_english_films_for_teens
+FROM films
+-- Filter to release_years to between 1990 and 1999
+WHERE release_year BETWEEN 1990 AND 1999
+-- Filter to English-language films
+	AND language ='English'
+-- Narrow it down to G, PG, and PG-13 certifications
+	AND certification IN ( 'G', 'PG', 'PG-13');
