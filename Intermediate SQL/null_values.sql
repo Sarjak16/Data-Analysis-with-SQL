3081d4e9c17aa09668aad9ec1ccf37caee82cea1
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-18 20:58:07 +0545

    Create null_values.sql

---
+++ Intermediate SQL/null_values.sql
@@ -0,0 +1,9 @@
+-- 1
+-- Select the title of every film that doesn't have a budget associated with it and use the alias no_budget_info.
+-- List all film titles with missing budgets
+SELECT title AS no_budget_info
+FROM films
+WHERE budget IS NULL;
+
+-- 2
+-- Count the number of films with a language associated with them and use the alias count_language_known.
