75c5feaa4b6955de7105b199e3e1b2ae40173481
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-22 21:27:42 +0545

    Update Sumarizing_data.sql

--- Intermediate SQL/Sumarizing_data.sql
+++ Intermediate SQL/Sumarizing_data.sql
@@ -5,6 +5,10 @@ SELECT SUM(duration) AS total_duration
 FROM films;
 -- 2
 -- Calculate the average duration of all films and alias with average_duration.
+-- Calculate the average duration of all films
+SELECT AVG(duration) AS average_duration
+FROM films;
+
 -- 3
 -- Find the most recent release_year in the films table, aliasing as latest_year.
 -- 4
