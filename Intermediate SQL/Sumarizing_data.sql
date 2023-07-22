9f27f2f045365e2d2e3c0971cfa71aa1a3b54296
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-22 21:27:02 +0545

    Create Sumarizing_data.sql

---
+++ Intermediate SQL/Sumarizing_data.sql
@@ -0,0 +1,11 @@
+-- 1
+-- Use the SUM() function to calculate the total duration of all films and alias with total_duration.
+-- Query the sum of film durations
+SELECT SUM(duration) AS total_duration
+FROM films;
+-- 2
+-- Calculate the average duration of all films and alias with average_duration.
+-- 3
+-- Find the most recent release_year in the films table, aliasing as latest_year.
+-- 4
+-- Find the duration of the shortest film and use the alias shortest_film.
