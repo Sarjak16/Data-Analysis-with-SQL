21b074a8824ef50b4d6e2314ad847f6ae5df6485
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-07-30 19:48:25 +0545

    Update Sumarizing_data.sql

--- Intermediate SQL/Sumarizing_data.sql
+++ Intermediate SQL/Sumarizing_data.sql
@@ -57,3 +57,10 @@ WHERE release_year BETWEEN 2000 AND 2012;
 -- Round the average number of facebook_likes to one decimal place
 SELECT ROUND(AVG(facebook_likes),1) AS avg_facebook_likes
 FROM reviews
+
+
+
+--Calculate the average budget from the films table, aliased as avg_budget_thousands, and round to the nearest thousand.
+-- Calculate the average budget rounded to the thousands
+SELECT ROUND(AVG(budget),-3) AS avg_budget_thousands
+FROM films
