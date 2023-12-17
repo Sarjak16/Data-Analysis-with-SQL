761fccbc9bfdd9eb03f79c1c3625339fc65257b3
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-17 23:49:39 +0545

    Update Basics.sql

---
+++ Basic_commands/Basics.sql
@@ -0,0 +1,20 @@
+-- SELECT&FROM
+
+SELECT studentID, FirstName, LastName, FirstName + ' ' + LastName AS FullName
+FROM student;
++-----------+-------------------+------------+------------------------+
+| studentID | FirstName         | LastName   | FullName               |
++-----------+-------------------+------------+------------------------+
+|         1 | Monique           | Davis      | Monique Davis          |
+|         2 | Teri              | Gutierrez  | Teri Gutierrez         |
+|         3 | Spencer           | Pautier    | Spencer Pautier        |
+|         4 | Louis             | Ramsey     | Louis Ramsey           |
+|         5 | Alvin             | Greene     | Alvin Greene           |
+|         6 | Sophie            | Freeman    | Sophie Freeman         |
+|         7 | Edgar Frank "Ted" | Codd       | Edgar Frank "Ted" Codd |
+|         8 | Donald D.         | Chamberlin | Donald D. Chamberlin   |
+|         9 | Raymond F.        | Boyce      | Raymond F. Boyce       |
++-----------+-------------------+------------+------------------------+
+9 rows in set (0.00 sec)
+
+
