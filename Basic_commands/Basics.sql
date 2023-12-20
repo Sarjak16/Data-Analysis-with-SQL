33f8a429ff47fb0d9d57c76fda0e4b95d8d85b8b
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-20 23:36:05 +0545

    Update Basics.sql

--- Basic_commands/Basics.sql
+++ Basic_commands/Basics.sql
@@ -38,10 +38,3 @@ ADD column_name datatype;
 -- The following SQL creates a CHECK constraint on the “Age” column when the “Persons” table is created. 
 --The CHECK constraint ensures that you can not have any person below 18 years.
 
-CREATE TABLE Persons (
-    ID int NOT NULL,
-    LastName varchar(255) NOT NULL,
-    FirstName varchar(255),
-    Age int,
-    CHECK (Age>=18)
-);
