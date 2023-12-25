0c0f6519f0003db419775466a983936f7798d7e5
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-25 23:47:00 +0545

    Update Basics.sql

--- Basic_commands/Basics.sql
+++ Basic_commands/Basics.sql
@@ -56,3 +56,26 @@ CREATE TABLE Persons (
     City varchar(255),
     CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Sandnes')
 );
+
+WHERE
+(AND, OR, IN, BETWEEN, and LIKE)
+
+The WHERE clause is used to limit the number of rows returned.
+
+As an example, first we will show you a SELECT statement and results without a WHERE statement. Then we will add a WHERE statement that uses all five qualifiers above.
+
+SELECT studentID, FullName, sat_score, rcd_updated FROM student;
++-----------+------------------------+-----------+---------------------+
+| studentID | FullName               | sat_score | rcd_updated         |
++-----------+------------------------+-----------+---------------------+
+|         1 | Monique Davis          |       400 | 2017-08-16 15:34:50 |
+|         2 | Teri Gutierrez         |       800 | 2017-08-16 15:34:50 |
+|         3 | Spencer Pautier        |      1000 | 2017-08-16 15:34:50 |
+|         4 | Louis Ramsey           |      1200 | 2017-08-16 15:34:50 |
+|         5 | Alvin Greene           |      1200 | 2017-08-16 15:34:50 |
+|         6 | Sophie Freeman         |      1200 | 2017-08-16 15:34:50 |
+|         7 | Edgar Frank "Ted" Codd |      2400 | 2017-08-16 15:35:33 |
+|         8 | Donald D. Chamberlin   |      2400 | 2017-08-16 15:35:33 |
+|         9 | Raymond F. Boyce       |      2400 | 2017-08-16 15:35:33 |
++-----------+------------------------+-----------+---------------------+
+9 rows in set (0.00 sec)
