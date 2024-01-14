b7f68315067769e528cc4f86bfce08308ada9044
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-01-14 20:55:38 +0545

    Update Basics.sql

--- Basic_commands/Basics.sql
+++ Basic_commands/Basics.sql
@@ -189,3 +189,26 @@ SELECT user_only_num1 AS AgeOfServer, (user_only_num1 - warranty_period) AS NonW
 |         36  |                     24 | 
 |         24  |                     12 | 
 +-------------+------------------------+
+
+
+-- You can also use AS to assign a name to a table to make it easier to reference in joins.
+
+SELECT ord.product, ord.ord_number, ord.price, cust.cust_name, cust.cust_number FROM customer_table AS cust
+
+JOIN order_table AS ord ON cust.cust_number = ord.cust_number
+-- This results in output as below.
+
++-------------+------------+-----------+-----------------+--------------+
+| product     | ord_number | price     | cust_name       | cust_number  |
++-------------+------------+-----------+-----------------+--------------+
+|     RAM     |   12345    |       124 | John Smith      |  20          |
+|     CPU     |   12346    |       212 | Mia X           |  22          |
+|     USB     |   12347    |        49 | Elise Beth      |  21          |
+|     Cable   |   12348    |         0 | Paul Fort       |  19          |
+|     Mouse   |   12349    |        66 | Nats Back       |  15          |
+|     Laptop  |   12350    |       612 | Mel S           |  36          |
+|     Keyboard|   12351    |        24 | George Z        |  95          |
+|     Keyboard|   12352    |        24 | Ally B          |  55          |
+|     Air     |   12353    |        12 | Maria Trust     |  11          |
++-------------+------------+-----------+-----------------+--------------+
+
