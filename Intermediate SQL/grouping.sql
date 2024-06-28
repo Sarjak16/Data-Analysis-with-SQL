7254df32abb50ba4a920993f0101afbe0b8f0a43
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-06-28 22:06:06 +0545

    Update grouping.sql

--- Intermediate SQL/grouping.sql
+++ Intermediate SQL/grouping.sql
@@ -14,3 +14,17 @@ GROUP BY release_year;
 SELECT release_year,country, MAX(budget) AS max_budget
 FROM films
 GROUP BY release_year, country; 
+
+
+--FILTERING GROUPED DATA----------------------------
+
+
+
+-- Select the country and distinct count of certification as certification_count
+SELECT country, COUNT(DISTINCT certification) AS certification_count
+FROM films
+
+-- Group by country
+GROUP BY country
+-- Filter results to countries with more than 10 different certifications
+HAVING COUNT(DISTINCT certification)>10;
